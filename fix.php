7866baca4a05937bdf85a1b504d853402ca7239b
cengiz akcan <info@cengizakcan.com>
2017-10-03 23:59:48 +0300

    Add files via upload

--- fix.php
+++ fix.php
@@ -1,5 +1,30 @@
 <?php
 
+/*
+     MIT License
+
+    Copyright (c) 2017 FixFramework
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+*/
+
     define("FIX_START_MICRO_TIME",      microtime(true));
     define('FIX_CLI',                   PHP_SAPI == 'cli' ? true : false);
     define('FIX_WIN',                   strpos(PHP_OS, 'WIN') !== false);
@@ -32,7 +57,7 @@
     define("FIX_VENDOR_FILE",           "System/Vendor/autoload.php");
     define("FIX_MEM",                   memory_get_usage());
 
-    require_once FIX_HOME_DIR . "/System/Autoloader/FIX_Autoloader.php";
+    require_once "System/Autoloader/FIX_Autoloader.php";
 
     new System\Kernel\FIX_Kernel();
 
